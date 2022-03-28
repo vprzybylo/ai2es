ef36003e933fe6062b057b3a3ece845ec779a9b0
vprzybylo <32338353+vprzybylo@users.noreply.github.com>
2022-03-28 14:45:20 -0400

    vectorize time match

---
+++ ai2es/parquet_time_match.py
@@ -0,0 +1,124 @@
+"""apply filters to NYSM parquet files by year"""
+import pandas as pd
+import os
+import numpy as np
+from datetime import datetime
+from typing import List
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+class DateMatch:
+    """match camera photos to closest mesonet observation in time for each station"""
+
+    def __init__(self, year: int, parquet_dir: str = "../mesonet_parquet_1M") -> None:
+        self.year = year
+        self.parquet_dir = parquet_dir
+        self.camera_df: pd.DataFrame = None
+        self.df: pd.DataFrame = None
+
+    def read_parquet(self) -> None:
+        """read parquet file holding mesonet data for a specified year (all stations)"""
+        self.df = (
+            pd.read_parquet(f"{self.parquet_dir}/{self.year}.parquet")
+            .set_index(["datetime"])
+            .sort_values(by=["datetime"])
+        )
+
+    def camera_photo_paths(self, photo_dir: str = "../cam_photos") -> None:
+        """create dataframe of camera images including station ids, paths, and datetimes
+
+        Args:
+            photo_dir (str, optional): directory of camera photos. Defaults to "../cam_photos".
+        """
+        photo_files = os.listdir(photo_dir)
+        stations = [photo.split("_")[1].split(".")[0] for photo in photo_files]
+        dates = [
+            datetime.strptime(photo.split("_")[0].replace("T", ""), "%Y%m%d%H%M%S")
+            for photo in photo_files
+        ]
+        self.camera_df = pd.DataFrame({"stid": stations, "datetime": dates})
+        # truncate for year
+        self.camera_df = self.camera_df[
+            self.camera_df["datetime"].dt.year == self.year
+        ].set_index(["datetime"])
+
+    def check_time_diff(
+        self, nysm_time: datetime, image_time: datetime, time_diff: int = 5
+    ) -> List[bool]:
+        """ensure that the time matching between the camera time
+        (image_time) and the nysm obs time (nysm_time) is less than time_diff minutes
+
+        Args:
+            nysm_time (pandas._libs.tslibs.timestamps): time of nysm observation
+            image_time (pandas._libs.tslibs.timestamps): time image was taken
+            time_diff (int, optional): time difference allowed in minutes between when
+                                       the image was taken and when the mesonet observation was recorded.
+                                       Defaults to 5.
+        """
+        time_delta_mins = divmod((image_time - nysm_time).total_seconds(), 60)[0]
+        return time_delta_mins < time_diff
+
+    def find_closest_date(self, time_diff=5) -> None:
+        """
+        for each datetime in the camera photo df, find the closest date in the mesonet df
+        - vectorized
+        """
+        # eventually will hold the matched camera paths based on time and station
+        self.df["camera path"] = np.nan
+
+        # first group camera df by station id
+        # there can be multiple identical times of images at diff stations
+        camera_stn = self.camera_df.groupby("stid")
+        nysm_stn = self.df.groupby("stid")
+        all_stn_groups = []
+        for (stn, group_cam), (_, group_nysm) in zip(camera_stn, nysm_stn):
+
+            # get the indices of the nysm datetimes at the closest match to the image datetimes
+            matched_date_indices = group_nysm.index.get_indexer(
+                group_cam.index, method="nearest"
+            )
+
+            # check that there is less than a 5 min time diff between when the image
+            # was taken and the matched nysm observation timestamp
+            time_diff = self.check_time_diff(
+                group_nysm.index[matched_date_indices],
+                group_cam.index,
+            )
+
+            group_cp = (
+                group_nysm.copy()
+            )  # to get rid of warning: "A value is trying to be set on a copy of a slice from a DataFrame"
+            # remove any indices where time match is too far apart
+            matched_date_indices = matched_date_indices[time_diff]
+
+            # assign datetime that image was taken to nysm df where time_diff returns true (i.e., close enough match)
+            group_cp["camera path"].iloc[matched_date_indices] = group_cam.index[
+                time_diff
+            ]
+
+            # append this station grouping out to save
+            all_stn_groups.append(group_cp)
+
+        len_before = len(self.df)
+        # remove rows in nysm df that don't have corresponding image
+        self.df = pd.concat(all_stn_groups).dropna(subset=["camera path"])
+        print("[INFO] Length of observations and matched images: ", len(self.df))
+        print(
+            f"[INFO] Removed {len_before - len(self.df)} rows of data that don't have a corresponding image"
+        )
+
+
+def main() -> None:
+
+    for year in range(2017, 2021):
+        print(f"[INFO] processing {year}..")
+        match = DateMatch(year)
+        match.read_parquet()
+        match.camera_photo_paths()
+        match.find_closest_date()
+
+
+if __name__ == "__main__":
+    main()
