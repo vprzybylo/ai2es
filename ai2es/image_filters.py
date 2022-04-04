6bfb1542c63aadc1c5a32cc63b066bd0e229b6d3
Vanessa <vprzybylo@albany.edu>
2022-04-04 17:27:50 +0000

    progress bar for day_or_night.py

--- ai2es/image_filters.py
+++ ai2es/image_filters.py
@@ -1,44 +1,41 @@
 """Apply filters to image in df for a given year"""
 import cv2
 import pandas as pd
+import numpy as np
 from dataclasses import dataclass
+import time
+from PIL import Image
+from parallelbar import progress_map
 
 
 @dataclass
 class ImageFilter:
-    """partition yearly df of images for day, night, precip, no precip, etc."""
+    """append yearly df of images for day, night, precip, no precip, etc."""
 
     year: int
+    df: pd.DataFrame = None
 
-    def read_df(self):
+    def read_parquet(self):
         """read parquet file that has yearly df of images"""
+        start_time = time.time()
         self.df = pd.read_parquet(f"/ai2es/matched_parquet/{str(self.year)}.parquet")
+        print(
+            f"[INFO] Read {self.year} parquet file in {time.time()-start_time} seconds."
+        )
 
-    def day(self, img_path: str):
-        """find images taken during the day"""
-        image = cv2.imread(img_path)
+    def time_of_day(self, file):
+        """find if images were taken during the day or night"""
+
+        image = np.asarray(Image.open(file))
         b, g, r = image[:, :, 0], image[:, :, 1], image[:, :, 2]
-        return None if (b == g).all() and (b == r).all() else True
-
-    def night(self):
-        """find images taken at night"""
-
-    def precip(self) -> None:
-        # CHANGE VAR FOR 1M
-        """find images where there IS precipitation occuring according to 5 min difference in mesonet data"""
-        self.df = self.df[self.df["precip_5min"] > 0.0]
-
-    def no_precip(self) -> None:
-        """find images where there is NO precipitation occuring"""
-        self.df = self.df[self.df["precip_5min"] == 0.0]
-
-    def station_filter(self, station: str) -> None:
-        """find images from a specific station id
-
-        Args:
-            station (str): station id
-        """
-        self.df = self.df[self.df["stid"] == station]
+        if (b == g).all() and (b == r).all():
+            is_night = True
+        else:
+            is_night = False
+            # if row["precip_accum_1min [mm]"] > 0.5 and is_night:
+            #     plt.imshow(image)
+            #     plt.show()
+        return is_night
 
     def grayscale(self, img_path: str):  # -> ndarray[int, int]:
         """convert image to grayscale
@@ -54,19 +51,21 @@ class ImageFilter:
 
 
 def main() -> None:
-    filt = ImageFilter(2017)
 
-    # pool = Pool(processes=8)
-    # time_of_day = pool.map(filt.day, filt.df['img_paths']) # or night()
-    # time_of_day = pool.map(filt.precip, filt.df['img_paths']) # or night()
+    year = 2020
+    filt = ImageFilter(year)
+    filt.read_parquet()
+    print(f'{len(filt.df["path"])} files to process')
+    is_night = progress_map(
+        filt.time_of_day,
+        filt.df["path"],
+        chunk_size=772153,
+        n_cpu=10,
+        core_progress=True,
+    )
 
-    # filt.precip()  # of no_precip
-    # filt.station_filter("TANN")
-    # filt.grayscale()
-
-    # print("[INFO] waiting for processes to finish...")
-    # pool.close()
-    # pool.join()
+    filt.df["night"] = is_night
+    filt.df.to_parquet(f"/ai2es/matched_parquet/{year}_timeofday.parquet")
 
 
 if __name__ == "__main__":
