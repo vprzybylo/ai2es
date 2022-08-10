7ed1f427c94c5e9b84f7547191527c87ea1cb243
Vanessa <vprzybylo@albany.edu>
2022-08-10 13:17:25 +0000

    mem errors watch.py

--- ai2es/watch.py
+++ ai2es/watch.py
@@ -3,13 +3,14 @@ import io
 import os
 import time
 from datetime import datetime
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 import cocpit
 import numpy as np
 import torch
 from cocpit import config as config
 from PIL import Image
+from PIL import UnidentifiedImageError
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers.polling import PollingObserver
 
@@ -29,7 +30,7 @@ class MonitorFolder(FileSystemEventHandler):
         self.w = csv.writer(self.csvfile, delimiter=",")
         self.b: cocpit.predictions.BatchPredictions = None
 
-    def check_night_image(self, filename: str) -> bool:
+    def check_night_image(self, filename: str) -> Optional[bool]:
         """
         Only make a prediction on image at night
 
@@ -38,9 +39,12 @@ class MonitorFolder(FileSystemEventHandler):
         Returns
             (bool): True if night else False
         """
-        image = np.asarray(Image.open(filename))
-        b, g, r = image[:, :, 0], image[:, :, 1], image[:, :, 2]
-        return bool((b == g).all() and (b == r).all())
+        try:
+            image = np.asarray(Image.open(filename))
+            b, g, r = image[:, :, 0], image[:, :, 1], image[:, :, 2]
+            return bool((b == g).all() and (b == r).all())
+        except UnidentifiedImageError:
+            print("couldn't find file: {filename}")
 
     def write_csv(self, event: FileSystemEventHandler):
         """
@@ -68,34 +72,34 @@ class MonitorFolder(FileSystemEventHandler):
         Args:
             event (FileSystemEventHandler): Event representing file/directory creation.
         """
-        # print(event.src_path)
-        test_data = cocpit.data_loaders.TestDataSet(
-            open_dir="",
-            file_list=[event.src_path],
-        )
 
         if self.check_night_image(event.src_path):
+            test_data = cocpit.data_loaders.TestDataSet(
+                open_dir="",
+                file_list=[event.src_path],
+            )
             test_loader = cocpit.data_loaders.create_loader(
                 test_data, batch_size=1, sampler=None
             )
-            for imgs, _ in test_loader:
+            with torch.no_grad():
+                for imgs, _ in test_loader:
 
-                self.b = cocpit.predictions.BatchPredictions(
-                    imgs, torch.load(config.MODEL_PATH)
-                )
-                with torch.no_grad():
+                    self.b = cocpit.predictions.BatchPredictions(
+                        imgs, torch.load(config.MODEL_PATH)
+                    )
                     self.b.find_max_preds()
                     self.b.top_k_preds(top_k_preds=3)
-                # print(
-                #     path,
-                #     config.CLASS_NAMES[self.b.classes[0]],
-                #     f"{self.b.probs[0]* 100:.2f}",
-                #     f"{self.b.probs[1]* 100:.2f}",
-                #     f"{self.b.probs[2]* 100:.2f}",
-                # )
+                    # print(
+                    #     path,
+                    #     config.CLASS_NAMES[self.b.classes[0]],
+                    #     f"{self.b.probs[0]* 100:.2f}",
+                    #     f"{self.b.probs[1]* 100:.2f}",
+                    #     f"{self.b.probs[2]* 100:.2f}",
+                    # )
 
-                self.write_csv(event)
-                torch.cuda.empty_cache()
+                    self.write_csv(event)
+                    del self.b
+                    torch.cuda.empty_cache()
 
 
 def current_date() -> str:
